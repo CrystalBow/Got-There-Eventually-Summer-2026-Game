5c33b4a2f81f82abb08d50183ae24c8023781b5d
Aidan Alich <aidanalich@gmail.com>
2026-07-16 04:00:32 -0700

    Add EnemyTurn and TopofRound State
    Yippee!!!!!!

--- Tolls and Taxes/Assets/Scripts/Combat/States/EnemyTurn.cs
+++ Tolls and Taxes/Assets/Scripts/Combat/States/EnemyTurn.cs
@@ -1,20 +1,78 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : State
+public class EnemyTurn : State
 {
-
+    CombatCenter combatCenter;
     public override void EnterState()
     {
-        throw new System.NotImplementedException();
+        Owner = this.GetComponent<CombatCenter>();
+        combatCenter = Owner as CombatCenter;
+        // throw new System.NotImplementedException();
     }
 
     public override void ExitState()
     {
-        throw new System.NotImplementedException();
+        Destroy(this);
+        // throw new System.NotImplementedException();
     }
 
     public override void UpdateState()
     {
         throw new System.NotImplementedException();
+    }
+
+    public void performEnemyTurn()
+    {
+        // Get an index of all living allies
+        List<int> alliedIndices = new List<int>();
+
+        for (int i = 0; i < combatCenter.initiativeOrder.Count; i++)
+        {
+            if ((combatCenter.initiativeOrder[i].isAlly == true) && (combatCenter.initiativeOrder[i].Reference.isDead() == false))
+            {
+                alliedIndices.Add(i);
+            }
+        }
+
+        int attackTarget = Random.Range(0, alliedIndices.Count);
+
+        combatCenter.initiativeOrder[alliedIndices[attackTarget]].Reference.damage(combatCenter.initiativeOrder[combatCenter.turnPosition].Reference.StaticData.Attack);
+
+        if (combatCenter.aliveAllies == 0)
+        {
+            // Do scene transition here for loss
+        }
+
+        bool foundDeadGuy = true;
+        // Need to increment first to properly check for out of bounds
+        combatCenter.turnPosition += 1;
+
+        while (foundDeadGuy == true)
+        {
+            // if out of bounds, the round is over so go to top of round
+            if (combatCenter.turnPosition >= combatCenter.initiativeOrder.Count)
+            {
+                ChangeState(this.AddComponent<TopofRound>());
+            }
+
+            foundDeadGuy = combatCenter.initiativeOrder[combatCenter.turnPosition].Reference.isDead();
+
+            if (foundDeadGuy == true)
+            {
+                combatCenter.turnPosition += 1;
+            }
+        }
+
+        if (combatCenter.initiativeOrder[combatCenter.turnPosition].isAlly == true)
+        {
+            ChangeState(this.AddComponent<PlayerTurn>());
+        }
+        else
+        {
+            ChangeState(this.AddComponent<EnemyTurn>());
+        }
     }
 }
--- Tolls and Taxes/Assets/Scripts/Combat/States/TopofRound.cs
+++ Tolls and Taxes/Assets/Scripts/Combat/States/TopofRound.cs
@@ -1,16 +1,87 @@
+using JetBrains.Annotations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TopofRound : State {
-    
+
+    CombatCenter combatCenter;
     
     public override void EnterState()
     {
-        throw new System.NotImplementedException();
+        Owner = this.GetComponent<CombatCenter>();
+        combatCenter = Owner as CombatCenter;
+
+        sortInitiativeList();
+
+        if (combatCenter.aliveEnemies == 0)
+        {
+            // do something for victory
+        }
+        else if (combatCenter.aliveAllies == 0)
+        {
+            // do something for getting your ass beat
+        }
+
+        combatCenter.turnPosition = 0;
+
+        bool foundDeadGuy = true;
+
+        while (foundDeadGuy == true)
+        {
+            foundDeadGuy = combatCenter.initiativeOrder[combatCenter.turnPosition].Reference.isDead();
+
+            if (foundDeadGuy == true)
+            {
+                combatCenter.turnPosition += 1;
+            }
+        }
+
+        if (combatCenter.initiativeOrder[combatCenter.turnPosition].isAlly == true)
+        {
+            ChangeState(this.AddComponent<PlayerTurn>());
+        }
+        else
+        {
+            ChangeState(this.AddComponent<EnemyTurn>());
+        }
+        // throw new System.NotImplementedException();
+    }
+
+    public void sortInitiativeList()
+    {
+        List<CombatCenter.InitiativeToken> toSort = combatCenter.initiativeOrder;
+        List<CombatCenter.InitiativeToken> toReturn = new List<CombatCenter.InitiativeToken>();
+
+        int originalCount = toSort.Count;
+
+        for (int i = 0; i < originalCount; i++)
+        {
+            int topSpeed = -1;
+            int indexOfFastest = -1;
+
+            for(int j = 0; j < toSort.Count; j++)
+            {
+                if (toSort[j].Reference.StaticData.Speed > topSpeed)
+                {
+                    topSpeed = toSort[j].Reference.StaticData.Speed;
+                    indexOfFastest = j;
+                }
+            }
+
+            toReturn.Add(toSort[indexOfFastest]);
+            toSort.RemoveAt(indexOfFastest);
+        }
+
+        combatCenter.initiativeOrder = toReturn;
     }
 
     public override void ExitState()
     {
-        throw new System.NotImplementedException();
+        Destroy(this);
+        // throw new System.NotImplementedException();
     }
 
     public override void UpdateState()
